c48032163c1c8d3f73d1c7c21c55a8748b58d3eb
pabloarak <pablo.arak.vm@gmail.com>
2018-10-13 09:43:41 -0300

    Ilumina color e input de usuario

---
+++ assets/js/juego.js
@@ -0,0 +1,79 @@
+const celeste = document.getElementById('celeste');
+const violeta = document.getElementById('violeta');
+const naranja = document.getElementById('naranja');
+const verde = document.getElementById('verde');
+const btnEmpezar = document.getElementById('btnEmpezar');
+
+class Juego {
+
+  constructor() {
+    this.inicializar();
+    this.generarSecuencia();
+    this.siguienteNivel();
+  }
+
+  inicializar() {
+    this.elegirColor = this.elegirColor.bind(this); // El bind nos ata al this de la clase y sin el estariamos atados solo al this del color
+    btnEmpezar.classList.add('hide');
+    this.nivel = 1
+    this.colores = {
+      celeste,
+      violeta,
+      naranja,
+      verde
+    }
+  }
+
+  generarSecuencia() {
+    this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+  }
+
+  siguienteNivel() {
+    this.iluminarSecuencia();
+    this.agregarEventosClick();
+  }
+
+  transformarNumeroAColor(numero){
+    switch(numero){
+      case 0:
+        return 'celeste'
+      case 1:
+        return 'violeta'
+      case 2:
+        return 'naranja'
+      case 3:
+        return 'verde'
+    }
+  }
+
+  iluminarSecuencia(){
+    for(let i=0 ; i<this.nivel ; i++){
+      const color = this.transformarNumeroAColor(this.secuencia[i]); // con var queda el ultimo numero de la secuencia, se pisan
+      setTimeout(() => this.iluminarColor(color), 1000*i);
+    }
+  }
+
+  iluminarColor(color){
+    this.colores[color].classList.add('light');
+    setTimeout(() => this.apagarColor(color), 350);
+  }
+
+  apagarColor(color){
+    this.colores[color].classList.remove('light');
+  }
+
+  agregarEventosClick(){
+    this.colores.celeste.addEventListener('click', this.elegirColor);
+    this.colores.verde.addEventListener('click', this.elegirColor);
+    this.colores.violeta.addEventListener('click', this.elegirColor);
+    this.colores.naranja.addEventListener('click', this.elegirColor);
+  }
+
+  elegirColor(evento){
+
+  }
+}
+
+function empezarJuego() {
+  window.juego = new Juego()
+}
